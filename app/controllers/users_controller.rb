e48334db133346911879fc6e74c49eb07cac24c9
chris-wamae <chriswamae123@gmail.com>
2023-03-31 12:10:01 +0300

    Check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :allergy_type)
+    params.permit(:user).permit(:username, :email, :password, :allergy_type)
   end
 end
