16a713fab5387da355ecda3254b097862738052b
chris-wamae <chriswamae123@gmail.com>
2023-03-31 16:57:07 +0300

    Changed seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 puts "Seeding..."
 #create a User
-User.create(username:"wamae",password:"wamae",password_confirmation:"wamae",email:"chriswamae123@gmaill.com")
+User.create(username:"wamae",password:"wamae",password_confirmation:"wamae",email:"chriswamae123@gmail.com")
 
 Recipe.create(title:"Beef stew",
               description:"is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
