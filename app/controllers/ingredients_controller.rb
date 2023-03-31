aed8e3f572af0e462452d5623733465ea062778a
chris-wamae <chriswamae123@gmail.com>
2023-03-31 06:20:53 +0300

    Defined methods for Ingrdeients Controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,21 @@
 class IngredientsController < ApplicationController
+
+    def create 
+        recipe_ingredient = Ingredient.create(allowed_params)
+        render json: recipe_ingredient
+    end
+    
+    def destroy
+        delete_ingredient = Ingredient.find(params[:id])
+        delete_ingredient.destroy
+    end
+
+   
+    private
+       
+    def allowed_params
+        params.permit(:name,:recipe_id,:quantity,:measurement_unit,:calories)
+    end
+   
+
 end
