fdf90efb482726eab6134a1ee1f31080eedda1fc
chris-wamae <chriswamae123@gmail.com>
2023-03-31 17:37:17 +0300

    Reverted Application Controller changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,22 @@
 class ApplicationController < ActionController::Base
   include ActionController::Cookies
   skip_before_action :verify_authenticity_token
-  # helper_method :current_user
+  helper_method :current_user
 
-  # before_action :set_default_response_format
+  before_action :set_default_response_format
 
   private
 
-  # def set_default_response_format
-  #   request.format = :json
-  # end
+  def set_default_response_format
+    request.format = :json
+  end
 
-  # def current_user
-  #   @current_user ||= User.find_by(id: session[:user_id])
-  # end
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 
-  # def require_login
-  #  # unless current_user
-  #     render json: { error: "You must be logged in to access this resource" }, status: :unauthorized
-  # end
+  def require_login
+   # unless current_user
+      render json: { error: "You must be logged in to access this resource" }, status: :unauthorized
+  end
 end
