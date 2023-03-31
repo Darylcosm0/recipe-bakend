9f95095f22d404d325351dad41ca3df81f1bab04
chris-wamae <chriswamae123@gmail.com>
2023-03-31 06:10:48 +0300

    Added routes for Recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  #User routes
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   get "logout", to: "sessions#destroy"
@@ -6,4 +7,17 @@ Rails.application.routes.draw do
   post "signup", to: "registrations#create"
   get "account/edit", to: "registrations#edit", as: "edit_registration"
   get '/password_resets/validate_token', to: 'password_resets#validate_token', format: :json
+
+  #Recipe routes
+  resources :recipe_labels
+  resources :reviews
+  resources :labels
+  resources :recipes,except:[:destroy]
+  resources :ingredients, only:[:create,:destroy]
+  resources :allergies, only:[:create,:destroy,:index]
+  resources :user_recipes, only:[:create,:destroy]
+  get "/search", to: "recipes#search"
+  get "/sort-new", to: "recipes#sort_newest"
+  get "/sort-old", to: "recipes#sort_oldest"
+  delete "/recipe_labels", to: "recipe_labels#destroy"
 end
