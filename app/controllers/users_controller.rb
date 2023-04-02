ac2ad7c2fd61a1453b0c255fcad31a06ca2de448
chris-wamae <chriswamae123@gmail.com>
2023-04-02 15:17:03 +0300

    Changed user show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_login, except: [:new, :create]
+  before_action :require_login, except: [:new, :create,:show]
 
   # def new
   #   @user = User.new
